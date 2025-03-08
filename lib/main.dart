45685825388440b24890e6b6d9ae5af01cd59bea
Serik Dias <dias.sabitov.96@mail.ru>
2025-03-08 12:27:13 +0500

    mainScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:practic_app_course/screens/auth_screens/auth_screen.dart';
+// import 'package:practic_app_course/screens/auth_screens/auth_screen.dart';
+import 'package:practic_app_course/screens/main_screens/main_screen.dart';
 // import 'package:practic_app_course/screens/auth_screens/auth_screen.dart';
 // import 'package:practic_app_course/screens/main_screens/main_screen.dart';
 
 void main() {
   runApp(
-    const MaterialApp(debugShowCheckedModeBanner: false, home: AuthScreen()),
+    const MaterialApp(debugShowCheckedModeBanner: false, home: MainScreen()),
   );
 }
--- lib/screens/main_screens/main_screen.dart
+++ lib/screens/main_screens/main_screen.dart
@@ -1,11 +1,11 @@
-import 'package:practic_app_course/imports.dart';
+import 'package:flutter/material.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: CircleStack()));
+    return Scaffold(backgroundColor: Colors.blueAccent, body: CircleStack());
   }
 }
 
@@ -14,20 +14,30 @@ class CircleStack extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Stack(
       alignment: Alignment.center, // Центрируем все круги
       children: [
+        // Круги и градиенты, которые вы уже добавили
         OverflowBox(
           minWidth: 0,
           minHeight: 0,
           maxWidth: double.infinity,
           maxHeight: double.infinity,
           child: Container(
-            width: 788,
-            height: 788,
+            width: 800,
+            height: 800,
             decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 207, 243, 46),
               shape: BoxShape.circle,
+              gradient: RadialGradient(
+                center: Alignment.center,
+                radius: 1,
+                colors: [
+                  Color.fromRGBO(52, 124, 218, 0),
+                  Color.fromRGBO(52, 124, 218, 1),
+                ],
+                stops: [0.0, 1.0],
+              ),
             ),
           ),
         ),
@@ -37,11 +47,19 @@ class CircleStack extends StatelessWidget {
           maxWidth: double.infinity,
           maxHeight: double.infinity,
           child: Container(
-            width: 610,
-            height: 610,
+            width: 620,
+            height: 620,
             decoration: BoxDecoration(
-              color: AppColors.primaryColor,
               shape: BoxShape.circle,
+              gradient: RadialGradient(
+                center: Alignment.center,
+                radius: 1,
+                colors: [
+                  Color.fromRGBO(52, 124, 218, 0),
+                  Color.fromRGBO(52, 124, 218, 1),
+                ],
+                stops: [0.0, 1.0],
+              ),
             ),
           ),
         ),
@@ -51,11 +69,19 @@ class CircleStack extends StatelessWidget {
           maxWidth: double.infinity,
           maxHeight: double.infinity,
           child: Container(
-            width: 456,
-            height: 456,
+            width: 466,
+            height: 466,
             decoration: BoxDecoration(
-              color: Colors.green,
               shape: BoxShape.circle,
+              gradient: RadialGradient(
+                center: Alignment.center,
+                radius: 1,
+                colors: [
+                  Color.fromRGBO(52, 124, 218, 0),
+                  Color.fromRGBO(52, 124, 218, 1),
+                ],
+                stops: [0.0, 1.0],
+              ),
             ),
           ),
         ),
@@ -65,12 +91,297 @@ class CircleStack extends StatelessWidget {
           maxWidth: double.infinity,
           maxHeight: double.infinity,
           child: Container(
-            width: 305,
-            height: 305,
+            width: 315,
+            height: 315,
             decoration: BoxDecoration(
-              color: Colors.red,
               shape: BoxShape.circle,
+              gradient: RadialGradient(
+                center: Alignment.center,
+                radius: 1,
+                colors: [
+                  Color.fromRGBO(52, 124, 218, 0),
+                  Color.fromRGBO(52, 124, 218, 1),
+                ],
+                stops: [0.0, 1.0],
+              ),
             ),
+          ),
+        ),
+        // Здесь вы поместили аватарку с текстом в самый верхний слой
+        Align(
+          alignment: Alignment.topCenter,
+          child: Padding(
+            padding: const EdgeInsets.only(top: 50.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                CircleAvatar(
+                  radius: 60,
+                  backgroundImage: AssetImage(
+                    'assets/images/avatar1.png',
+                  ), // Путь к изображению
+                  backgroundColor: Colors.transparent,
+                ),
+                SizedBox(height: 8),
+                Text(
+                  'Диас, 21',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Color.fromRGBO(47, 132, 243, 1),
+                      ),
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.settings),
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(width: 17),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Color.fromRGBO(47, 132, 243, 1),
+                      ),
+                      child: TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Редактировать',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                margin: EdgeInsets.only(top: size.height * 0.3),
+                height: 540,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(24),
+                    topRight: Radius.circular(23),
+                  ),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(24),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          // виджет с белым фоном
+                          Container(
+                            width: 162,
+                            height: 120,
+                            decoration: BoxDecoration(
+                              color: Color(0xFFB3EED8),
+                              borderRadius: BorderRadius.circular(16),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black.withOpacity(0.1),
+                                  spreadRadius: 3,
+                                  blurRadius: 5,
+                                  offset: Offset(0, 3), // Сдвиг тени
+                                ),
+                              ],
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                    left: 14,
+                                    right: 10,
+                                    top: 15,
+                                  ),
+                                  child: Text(
+                                    'Помощь',
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                ),
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Padding(
+                                      padding: EdgeInsets.only(
+                                        left: 14,
+                                        right: 10,
+                                        top: 5,
+                                      ),
+                                      child: Text(
+                                        'Служба \nподдержки',
+                                        style: TextStyle(fontSize: 12),
+                                      ),
+                                    ),
+                                    Expanded(
+                                      child: Image.asset(
+                                        'assets/images/bird.png',
+                                        width: 68, // Размер изображения
+                                        height: 80, // Размер изображения
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            width: 162,
+                            height: 120,
+                            decoration: BoxDecoration(
+                              color: Color(0xFF9AE5FC),
+                              borderRadius: BorderRadius.circular(16),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black.withOpacity(0.1),
+                                  spreadRadius: 3,
+                                  blurRadius: 5,
+                                  offset: Offset(0, 3),
+                                ),
+                              ],
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                    left: 14,
+                                    right: 10,
+                                    top: 15,
+                                  ),
+                                  child: Text(
+                                    'FAQ',
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                ),
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Padding(
+                                      padding: EdgeInsets.only(
+                                        left: 14,
+                                        right: 10,
+                                        top: 5,
+                                      ),
+                                      child: Text(
+                                        'Частые \nвопросы',
+                                        style: TextStyle(fontSize: 12),
+                                      ),
+                                    ),
+                                    Expanded(
+                                      child: Image.asset(
+                                        'assets/images/iconstwo.png',
+                                        width: 68, // Размер изображения
+                                        height: 80, // Размер изображения
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 24),
+                        child: Container(
+                          padding: EdgeInsets.only(
+                            left: 14,
+                            right: 14,
+                            top: 14,
+                            bottom: 14,
+                          ),
+                          width: double.infinity,
+                          height: 120,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(16),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.1),
+                                blurRadius: 8,
+                                offset: const Offset(0, 4),
+                              ),
+                            ],
+                          ),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Полезные советы',
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.black87,
+                                      ),
+                                    ),
+                                    SizedBox(height: 8),
+                                    Text(
+                                      'Мы собрали для вас полезные \nлайфхаки и советы для общения.',
+                                      style: TextStyle(
+                                        fontSize: 14,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              const SizedBox(width: 16),
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(16),
+                                child: Image.asset(
+                                  'assets/images/present.png',
+                                  width: 50,
+                                  height: 50,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      // ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ],
